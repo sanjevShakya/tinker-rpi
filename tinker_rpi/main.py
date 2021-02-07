7cef6c810c196f793bcf477b62e37666226486be
Sanjeev Shakya <sanjevsakya@gmail.com>
2021-02-07 21:17:46 +0700

    add buzzer

--- tinker_rpi/main.py
+++ tinker_rpi/main.py
@@ -1,16 +1,20 @@
-from gpiozero import LED, Button
+from gpiozero import LED, Button, Buzzer
 from signal import pause
 
 led = LED(17)
 button = Button(2)
+buzzer = Buzzer(26)
 
-def led_on():
+def button_pressed():
     led.on()
+    buzzer.on()
 
-def led_off():
+
+def button_released():
     led.off()
+    buzzer.off()
 
-button.when_pressed = led_on
-button.when_released = led_off
+button.when_pressed = button_pressed
+button.when_released = button_released
 
 pause()
