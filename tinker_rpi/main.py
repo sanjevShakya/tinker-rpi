82d720b1aa0e70bf81cb44c548811a09975ce647
Sanjeev Shakya <sanjevsakya@gmail.com>
2021-02-07 21:21:55 +0700

    add motion sensor

--- tinker_rpi/main.py
+++ tinker_rpi/main.py
@@ -1,9 +1,10 @@
-from gpiozero import LED, Button, Buzzer
+from gpiozero import LED, Button, Buzzer, MotionSensor
 from signal import pause
 
 led = LED(17)
 button = Button(2)
 buzzer = Buzzer(26)
+pir = MotionSensor(19)
 
 def button_pressed():
     led.on()
@@ -14,7 +15,15 @@ def button_released():
     led.off()
     buzzer.off()
 
+def when_motion():
+    print('Motion detected')
+
+def when_no_motion():
+    print('No motion available')
+
 button.when_pressed = button_pressed
 button.when_released = button_released
+pir.when_motion = when_motion
+pir.when_no_motion = when_no_motion
 
 pause()
