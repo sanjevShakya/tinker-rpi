be6485aa35df06e2712142359fd074e364e26ffb
Sanjeev Shakya <sanjevsakya@gmail.com>
2021-02-08 09:56:42 +0700

    check led status

--- tinker_rpi/main.py
+++ tinker_rpi/main.py
@@ -1,63 +1,94 @@
-from gpiozero import LED, PWMLED, Button, Buzzer, MotionSensor, LightSensor, MCP3008
-from signal import pause
-from threading import Thread
+from gpiozero import LED
+# , PWMLED, Button, Buzzer, MotionSensor, LightSensor, MCP3008
+# from signal import pause
 from time import sleep
 
-led = LED(17)
-pwm_led = PWMLED(21)
-button = Button(2)
-buzzer = Buzzer(26)
-pir = MotionSensor(19)
-lightSensor = LightSensor(13)
-pot = MCP3008(channel=0)
+led_pin_outs = [21,20,16, 12]
 
+def start():
+    return True;
 
-def button_pressed():
-    led.on()
-    buzzer.on()
+def ready():
+    return True;
 
+def surveillance():
+    return True;
 
-def button_released():
-    led.off()
-    buzzer.off()
+def alarm():
+    return True;
 
+def initialize_leds():
+    leds = [];
+    for led_pin in led_pin_outs:
+        leds.push(LED(led_pin))
 
-def when_motion():
-    print('Motion detected')
+    return leds
 
-
-def when_no_motion():
-    print('No motion available')
-
-def button_motion_handling():
-    pwm_led.source = lightSensor
-    print('Light Sensor value', lightSensor.value)
-    print('Pot Value', pot.value)
-    button.when_pressed = button_pressed
-    button.when_released = button_released
-    pir.when_motion = when_motion
-    pir.when_no_motion = when_no_motion
-    pause()
-
-
-def main_loop():
-    current_pot_value = pot.value
+def loop():
+    leds = initialize_leds();
     while True:
-        if(current_pot_value != pot.value):
-            print('Pot value changed', pot.value)
-            current_pot_value = pot.value
-            sleep(.5)
+        for led in leds:
+            led.on()
+            sleep(.2)
+            led.off()
+            sleep(.2)
 
-t1 = Thread(target=button_motion_handling)
-threads = [t1]
-t2 = Thread(target=main_loop)
-threads += [t2]
 
-t1.start()
-t2.start()
+# led = LED(17)
+# pwm_led = PWMLED(21)
+# button = Button(2)
+# buzzer = Buzzer(26)
+# pir = MotionSensor(19)
+# lightSensor = LightSensor(13)
+# pot = MCP3008(channel=0)
 
-for tloop in threads:
-    tloop.join()
+
+# def button_pressed():
+#     led.on()
+#     buzzer.on()
+
+
+# def button_released():
+#     led.off()
+#     buzzer.off()
+
+
+# def when_motion():
+#     print('Motion detected')
+
+
+# def when_no_motion():
+#     print('No motion available')
+
+# def button_motion_handling():
+#     pwm_led.source = lightSensor
+#     print('Light Sensor value', lightSensor.value)
+#     print('Pot Value', pot.value)
+#     button.when_pressed = button_pressed
+#     button.when_released = button_released
+#     pir.when_motion = when_motion
+#     pir.when_no_motion = when_no_motion
+#     pause()
+
+
+# def main_loop():
+#     current_pot_value = pot.value
+#     while True:
+#         if(current_pot_value != pot.value):
+#             print('Pot value changed', pot.value)
+#             current_pot_value = pot.value
+#             sleep(.5)
+
+# t1 = Thread(target=button_motion_handling)
+# threads = [t1]
+# t2 = Thread(target=main_loop)
+# threads += [t2]
+
+# t1.start()
+# t2.start()
+
+# for tloop in threads:
+#     tloop.join()
 
 
 
