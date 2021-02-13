058ca0ae6d68eda3a5918d83a12ceca4af9c54f7
Sanjeev Shakya <sanjevsakya@gmail.com>
2021-02-13 16:35:39 +0700

    refactor Part 2

--- tinker_rpi/main.py
+++ tinker_rpi/main.py
@@ -1,59 +1,36 @@
 from time import sleep
 from tinker_rpi.timer import Timer
-from tinker_rpi.utils import linear_eq
 from tinker_rpi.led_util import LEDUtil
 from gpiozero import Buzzer, LightSensor, Button, MotionSensor, MCP3008
-from tinker_rpi.constants import states, LED_PIN_OUTS, MOTION_SENSOR_PIN, BUZZER_PIN, LIGHT_SENSOR_PIN, DEFAULT_BTN_HOLD_TIME, BUTTON_PIN
+from tinker_rpi.utils import turn_buzzer, map_pot_value_to_seconds
+from tinker_rpi.constants import ALARM, READY, SURVEILLANCE, states, LED_PIN_OUTS, MOTION_SENSOR_PIN, BUZZER_PIN, LIGHT_SENSOR_PIN, DEFAULT_BTN_HOLD_TIME, BUTTON_PIN
 
 pir_timer = Timer()
 pot = MCP3008(channel=0)
 pir = MotionSensor(MOTION_SENSOR_PIN)
-
-def map_pot_value_to_seconds(pot_value):
-    if not pot_value:
-        return DEFAULT_BTN_HOLD_TIME
-    time = round(linear_eq(-1.8, pot_value, 2), 1)
-    return time
-
-def turn_buzzer(buzzer):
-    buzzer.on()
-    sleep(0.2)
-    buzzer.off()
-    sleep(0.2)
 
 
 def main():
     motion_count = 0
     buzzer = Buzzer(BUZZER_PIN)
     ledUtil = LEDUtil(LED_PIN_OUTS)
-    current_state = states["READY"]
+    current_state = states.get(READY)
     light_sensor = LightSensor(LIGHT_SENSOR_PIN)
     button_press_length = DEFAULT_BTN_HOLD_TIME
     button = Button(BUTTON_PIN, hold_time=DEFAULT_BTN_HOLD_TIME)
 
-    def alarm():
-        print("Alarm Mode")
-        turn_buzzer(buzzer)
-        ledUtil.led_alarm_sequence()
-        if button.is_pressed:
-            return states["READY"]
-        return states["ALARM"]
-
-    def surveillance():
-        nonlocal motion_count
-        print('Surveillance Active')
-        ledUtil.led_surveillance_sequence()
-        if pir.motion_detected:
-            print("Time ellapsed", pir_timer.get_ellapsed_time())
-            motion_count = motion_count + 1
-        if pir_timer.get_ellapsed_time() <= 15 and motion_count >= 2:
-            motion_count = 0
-            pir_timer.stop()
-            return states["ALARM"]
-        elif pir_timer.get_ellapsed_time() > 15 and motion_count < 2:
-            pir_timer.restart()
-            motion_count = 0
-        return states["SURVEILLANCE"]
+    def start():
+        nonlocal current_state
+        while True:
+            if current_state == states.get(READY):
+                next_state = ready()
+                current_state = next_state
+            if current_state == states.get(SURVEILLANCE):
+                next_state = surveillance()
+                current_state = next_state
+            if current_state == states.get(ALARM):
+                next_state = alarm()
+                current_state = next_state
 
     def ready():
         nonlocal button_press_length
@@ -69,16 +46,31 @@ def main():
                 button_press_length))
             if(button.is_held):
                 pir_timer.start()
-                return states["SURVEILLANCE"]
-        return states["READY"]
+                return states.get(SURVEILLANCE)
+        return states.get(READY)
 
-    while True:
-        if(current_state == states["READY"]):
-            next_state = ready()
-            current_state = next_state
-        if(current_state == states["SURVEILLANCE"]):
-            next_state = surveillance(motion_count)
-            current_state = next_state
-        if(current_state == states["ALARM"]):
-            next_state = alarm()
-            current_state = next_state
+    def surveillance():
+        nonlocal motion_count
+        print('Surveillance Active')
+        ledUtil.led_surveillance_sequence()
+        if pir.motion_detected:
+            print("Time ellapsed", pir_timer.get_ellapsed_time())
+            motion_count = motion_count + 1
+        if pir_timer.get_ellapsed_time() <= 15 and motion_count >= 2:
+            motion_count = 0
+            pir_timer.stop()
+            return states.get(ALARM)
+        elif pir_timer.get_ellapsed_time() > 15 and motion_count < 2:
+            pir_timer.restart()
+            motion_count = 0
+        return states.get(SURVEILLANCE)
+
+    def alarm():
+        print("Alarm Mode")
+        turn_buzzer(buzzer)
+        ledUtil.led_alarm_sequence()
+        if button.is_pressed:
+            return states.get(READY)
+        return states.get(ALARM)
+
+    start()
--- tinker_rpi/utils.py
+++ tinker_rpi/utils.py
@@ -1,3 +1,25 @@
+from tinker_rpi.constants import DEFAULT_BTN_HOLD_TIME
+from time import sleep
+
+Y_INTERCEPT = 2
+ROUNDOFF_DECIMAL_PLACES = 1
+SLOPE_FOR_POTENTIOMETER = -1.8
+
 
 def linear_eq(m, x, c):
     return m * x + c
+
+
+def map_pot_value_to_seconds(pot_value):
+    if not pot_value:
+        return DEFAULT_BTN_HOLD_TIME
+    time = round(linear_eq(SLOPE_FOR_POTENTIOMETER, pot_value,
+                           Y_INTERCEPT), ROUNDOFF_DECIMAL_PLACES)
+    return time
+
+
+def turn_buzzer(buzzer):
+    buzzer.on()
+    sleep(0.2)
+    buzzer.off()
+    sleep(0.2)
